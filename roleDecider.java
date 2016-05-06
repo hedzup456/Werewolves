321fc4fc54559897e9e18e486f130124e02f7409
Richard Henry <richardhenry602@gmail.com>
2016-05-06 13:48:45 +0100

    More logic, plus tests.. which don't work

--- roleDecider.java
+++ roleDecider.java
@@ -11,20 +11,60 @@ public class roleDecider{
 		isLovers = (players.length > 8);
 	}
 
+	private String[] populateRolesArray(int numberOfWolves, int numberOfPlayers){	// Will only work with at least (numberOfWolves + 4) players
+		String[] rolesArray = new String[numberOfPlayers];
+
+		int numberOfCubs = numberOfWolves/2;	// About half, mostly adults though
+		for (int i=0; i < numberOfCubs; i++){
+			rolesArray[i] = "Werewolf cub"; 
+		}
+		for (int i = numberOfCubs; i < numberOfWolves; i++){
+			rolesArray[i] = "Werewolf";
+		}
+		rolesArray[numberOfWolves] = "Seer";	// One seer
+		rolesArray[numberOfWolves + 1] = "Trapper";
+		rolesArray[numberOfWolves + 2] = "Vigilante";
+		rolesArray[numberOfWolves + 3] = "Protector";
+
+		for (int i = numberOfWolves + 4; i < numberOfPlayers; i++){
+			rolesArray[i] = "Villager";
+		}
+
+		return rolesArray;
+	}
+
 	public String[][] decideRoles(){
 		String[][] roles = new String[numberOfPlayers][2];
 		Random random = new Random();
 		
 		int numberOfWolves = random.nextInt(numberOfPlayers/2); // No more than half the players are wolves!
+		while (numberOfWolves <= 0){
+			numberOfWolves = random.nextInt(numberOfPlayers/2);	
+		}
 		
-			
+		String[] rolesArray = populateRolesArray(numberOfWolves, numberOfPlayers);
+
 		for (int i = 0; i < numberOfPlayers; i++){
 			String player = players[i];
 			roles[i][0] = player;
+			String role = "USED";
+			while (role == "USED"){
+				System.out.println("Role used, repeating for " + player);
+				int randomIndex = random.nextInt(numberOfPlayers);
+				System.out.println(randomIndex);
+				role = rolesArray[randomIndex];
+				rolesArray[randomIndex] = "USED";
+			}
 		}
+		return roles;
 	}
 	public static void main(String[] args){
-		Random random = new Random();
-
+		String[] names = {"Richard", "Iman", "Ru", "Ellie", "Kajetan", "Willow", "A" , "B" , "C"};
+		roleDecider rd = new roleDecider(names);
+		
+		String[][] finalRoles = rd.decideRoles();
+		for (String[] player: finalRoles){
+			System.out.println(player[0] + " is a " + player[1]);
+		}
 	}
 }
